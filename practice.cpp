6ad150c6bd443535cc78886e3b6f7b106c8ffd78
Rakesh Biswas <rbiswas01999@gmail.com>
2025-08-17 13:31:47 +0600

    phoeninx and puzzle problem solved

--- practice.cpp
+++ practice.cpp
@@ -1,19 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool perfectSquare(int n)
+{
+    int x = sqrt(n);
+    return (x * x == n);
+}
+
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        int n, m, k;
-        cin >> n >> m >> k;
-        int result = (n - 1) + (m - 1) * n;
-        if (result != k)
-            cout << "NO" << endl;
-        else
+        int n;
+        cin >> n;
+        
+        if (n % 2 == 0 && perfectSquare(n / 2))
+        {
             cout << "YES" << endl;
+        }
+        else if (n % 4 == 0 && perfectSquare(n / 4))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
