06c6ee223f1a75e8ce4642422319097030d75b81
Rakesh Biswas <rbiswas01999@gmail.com>
2025-08-21 20:12:57 +0600

    code file , doc  updated

--- B_Lord_of_the_Values.cpp
+++ B_Lord_of_the_Values.cpp
@@ -1,45 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int t;cin >> t;
-    while (t--)
-    {
-        int n;cin >> n;
-        int ar[n + 4];
-        for (int i = 1; i <= n; i++)
-        {
-            cin >> ar[i];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; 
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        vector<array<int,3>> ops; // {type, i, j}
+
+        // Process in pairs
+        for (int i = 1; i <= n; i += 2) {
+            int j = i + 1;
+            // Apply 6-step fixed sequence
+            ops.push_back({2, i, j});
+            ops.push_back({1, i, j});
+            ops.push_back({2, i, j});
+            ops.push_back({1, i, j});
+            ops.push_back({2, i, j});
+            ops.push_back({1, i, j});
         }
-        vector<pair<int, pair<int, int>>> v;
-        for (int i = 1; i <= n; i += 2)
-        {
-            int a = i;
-            int b = i + 1;
-            if (ar[i] != ar[i + 1])
-            {
-                v.push_back({1, {a, b}});
-                v.push_back({2, {a, b}});
-                v.push_back({1, {a, b}});
-                v.push_back({1, {a, b}});
-                v.push_back({2, {a, b}});
-                v.push_back({1, {a, b}});
-            }
-            else
-            {
-                v.push_back({2, {a, b}});
-                v.push_back({2, {a, b}});
-                v.push_back({1, {a, b}});
-                v.push_back({1, {a, b}});
-            }
-        }
-        cout << v.size() << endl;
-        for (int i = 0; i < v.size(); i++)
-        {
-            cout << v[i].first << " " << v[i].second.first << " " << v[i].second.second << endl;
+
+        cout << ops.size() << "\n";
+        for (auto &op : ops) {
+            cout << op[0] << " " << op[1] << " " << op[2] << "\n";
         }
     }
-    
-    return 0;
 }
