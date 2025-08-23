6dcf823390981cda5cdc15d6d94055eeaa7dc563
Rakesh Biswas <rbiswas01999@gmail.com>
2025-08-24 03:17:43 +0600

    array reordering

--- B_Array_Reodering.cpp
+++ B_Array_Reodering.cpp
@@ -3,19 +3,19 @@ using  namespace  std;
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;cin>>t;
     while(t--)
     {
         int n,x;cin>>n;
         vector<int>v,v1;
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             cin>>x;
             if(x%2==0)v.push_back(x);
             else v1.push_back(x);
         }
-        int ans=0;
-        int z=1;
+        int ans=0, z=1;
         for(int i=0;i<v.size();i++)
         {
             ans+=n-z;
@@ -30,7 +30,6 @@ int main()
                 if(__gcd(v1[i],v1[j])>1)ans1++;
             }
         }
-
         cout<<ans+ans1<<endl;
     }
     return 0;
