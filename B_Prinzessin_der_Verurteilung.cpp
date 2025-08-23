f996e38c9f95cd2e80153bef5b67d1bbbe50d60f
Rakesh Biswas <rbiswas01999@gmail.com>
2025-08-24 03:14:58 +0600

    code  updated

--- B_Prinzessin_der_Verurteilung.cpp
+++ B_Prinzessin_der_Verurteilung.cpp
@@ -1,42 +1,37 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h>
+using  namespace  std;
 
-string str;
+string ss;
 void num_to_str(int num)
 {
-    if (num)
+    if(num)
     {
-        num_to_str((num - 1) / 26);
-        str += ('a' + (num - 1) % 26);
+        num_to_str((num-1)/26);
+        ss+=('a'+ (num-1)%26);
     }
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--)
+    int t;cin>>t;
+    while(t--)
     {
-        int n;
-        cin >> n;
-        string s;
-        cin >> s;
+        int n;cin>>n;
+        string s;cin>>s;
         string ans;
-        int i = 1;
-        while (1)
+        int i=1;
+        while(1)
         {
-            str = "";
+            ss="";
             num_to_str(i);
-            int pos = s.find(str);
-            if (pos == -1)
+            int pos=s.find(ss);
+            if(pos==-1)
             {
-                ans = str;
+                ans=ss;
                 break;
             }
             i++;
         }
-        cout << ans << endl;
+       cout<<ans<<endl;
     }
 }
