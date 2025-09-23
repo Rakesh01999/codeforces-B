53746c24fd2069dd60475b971cd632d98a18e593
Rakesh Biswas <rbiswas01999@gmail.com>
2025-09-23 18:55:23 +0600

    solution done

---
+++ B_Special_Numbers.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+using ll = long long;
+
+ll modpow(ll base, ll exp, ll mod) {
+    ll result = 1;
+    while (exp > 0) {
+        if (exp & 1) result = (result * base) % mod;
+        base = (base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin >> t;
+    while (t--) {
+        ll n, k;
+        cin >> n >> k;
+        ll ans = 0;
+        ll power = 1; 
+        while (k > 0) {
+            if (k & 1) ans = (ans + power) % MOD;
+            power = (power * n) % MOD;
+            k >>= 1;
+        }
+        cout << ans % MOD << endl ;
+    }
+
+    return 0;
+}
