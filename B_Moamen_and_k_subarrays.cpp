a4e1d85ff268c16db0370172d285730b242482e0
Rakesh Biswas <rbiswas01999@gmail.com>
2025-10-03 20:14:05 +0600

    new problem updated

--- B_Moamen_and_k_subarrays.cpp
+++ B_Moamen_and_k_subarrays.cpp
@@ -1,36 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+ 
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, k;
-        cin >> n >> k;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-        
-        // Create sorted copy
-        vector<int> sorted_a = a;
-        sort(sorted_a.begin(), sorted_a.end());
-        
-        // Map value -> index in sorted array
-        unordered_map<int,int> pos;
-        for (int i = 0; i < n; i++) pos[sorted_a[i]] = i;
-        
-        // Count segments
-        int segments = 1;
-        for (int i = 1; i < n; i++) {
-            if (pos[a[i]] != pos[a[i-1]] + 1) {
-                segments++;
-            }
+    int t ; cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+ 
+        map<int,int>mp;
+        int ar[n+3];
+ 
+        for(int i=0;i<n;i++){
+            cin>>ar[i];
+            mp[ar[i]]=i;
         }
-        
-        if (segments <= k) cout << "Yes"<<endl ;
-        else cout << "No" <<endl ;
+ 
+        sort(ar,ar+n);
+ 
+        int segment=1;
+        int ind=mp[ar[0]];
+        for(int i=1;i<n;i++){
+            int z=mp[ar[i]];
+            if(z!=ind+1)
+                segment++;
+            ind=z;
+        }
+
+        if(segment<=k)
+            cout<<"Yes"<<endl;
+        else
+            cout<<"No"<<endl;
+
     }
+
+
     return 0;
 }
+ 
