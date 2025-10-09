e91f8cc8bfad8741368e37b6bae091d13b558360
Rakesh Biswas <rbiswas01999@gmail.com>
2025-10-09 20:42:30 +0600

    new problem added

---
+++ B_Missing_Bigram.cpp
@@ -0,0 +1,34 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;cin >> t;
+    while (t--) {
+        int n;cin >> n;
+        vector<string> grams(n - 2);
+        for (int i = 0; i < n - 2; i++) 
+            cin >> grams[i];
+
+        string result = "";
+        result += grams[0][0];
+        
+        bool fixed = false;
+        for (int i = 0; i < n - 3; i++) {
+            result += grams[i][1];
+            if (grams[i][1] != grams[i + 1][0]) {
+                result += grams[i + 1][0];
+                fixed = true;
+            }
+        }
+        result += grams.back()[1];
+
+        if ((int)result.size() < n) 
+            result += 'a';
+
+        cout << result << "\n";
+    }
+
+    return 0;
+}
