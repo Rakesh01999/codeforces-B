4a52e190a466a4bab9a53a017662b4233f512dac
Rakesh Biswas <rbiswas01999@gmail.com>
2025-10-10 20:02:55 +0600

    code updated

--- B_Missing_Bigram.cpp
+++ B_Missing_Bigram.cpp
@@ -2,35 +2,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;cin >> t;
-    while (t--) {
-        int n;cin >> n;
-        vector<string> grams(n - 2);
-        for (int i = 0; i < n - 2; i++) 
-            cin >> grams[i];
-
-        string result = "";
-        result += grams[0][0];
-        
-        bool fixed = false;
-        for (int i = 0; i < n - 3; i++) {
-            result += grams[i][1];
-            if (grams[i][1] != grams[i + 1][0]) {
-                result += grams[i + 1][0];
-                fixed = true;
+int main()
+{
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        string s[n+4];
+        for(int i=0;i<n-2;i++)cin>>s[i];
+        string ans;
+        ans=s[0];
+        int cnt=0;
+        for(int i=1;i<n-2;i++){
+            int z=ans.size()-1;
+            if(ans[z]==s[i][0]){
+                ans+=s[i][1];
+            }else{
+                cnt=1;
+                ans+=s[i];
             }
         }
-        result += grams.back()[1];
-
-        if ((int)result.size() < n) 
-            result += 'a';
-
-        cout << result << "\n";
+        if(cnt==0)
+            ans+='a';
+        cout<<ans<<endl;
     }
-
     return 0;
 }
+
